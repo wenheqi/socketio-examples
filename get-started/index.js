676e1bb21c02499314d1d3d0e6ea4f837628cd0b
wenheqi <wenhe.qi@outlook.com>
2020-05-24 15:16:50 -0700

    Create get-started example
    This example is adapted from https://socket.io/get-started/chat/

---
+++ get-started/index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const socketio = require("socket.io");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use("/", express.static("public"));
+
+const server = app.listen(PORT, () => {
+  console.log(`server listening on PORT ${PORT}`);
+});
+
+const io = socketio(server);
+
+io.on("connection", (socket) => {
+  console.log(`socket ${socket.id} connected`);
+  socket.on("chat message", (msg) => {
+    // console.log(`msg is ${msg}`);
+    io.emit("chat message", msg);
+  });
+  socket.on("disconnect", () => {
+    console.log(`socket id ${socket.id} is disconnected`);
+  });
+});
